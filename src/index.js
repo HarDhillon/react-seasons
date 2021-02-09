3f376f99beb1ae9f15fb59733124e97ee10322b5
Har Dhillon <har7173@gmail.com>
2021-02-09 13:29:37 +0400

    initializing state

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 
 
 class App extends React.Component {
+
+  // Constructor is specific to JS not react. Called first in a class
+  constructor(props) {
+    // We do this to reference the parent constructor. We HAVE to do this every time
+    super(props);
+    // intitalizing the state. MUST be done when a component is created
+    this.state = { lat: null }; // default it to null
+  }
+
+  // We have to define render in react
   render() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
