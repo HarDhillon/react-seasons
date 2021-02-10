2377a05781b0b8e5aa5e61a7e485bb7dd5a03bd2
Har Dhillon <har7173@gmail.com>
2021-02-10 15:35:52 +0400

    importing our season display component and returning word based on users location and date

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay'
 
 
 class App extends React.Component {
@@ -10,15 +11,21 @@ class App extends React.Component {
     super(props);
     // intitalizing the state. MUST be done when a component is created
     // This the ONLY time to do direct assignment, else call setState.
-    this.state = { lat: null, errorMessage: "" }; // default it to null
+    this.state = { lat: null, errorMessage: "" };
+  }
 
+  // this function is called when rendered for first time
+  componentDidMount () {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
+      (position) => 
         // use setState to update state!
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => this.setState({ errorMessage: err.message })
-    );
+        this.setState({ lat: position.coords.latitude }), (err) => this.setState({ errorMessage: err.message })
+    )
+  }
+
+  // called when updates
+  componentDidUpdate () {
+    console.log("My component was just updated")
   }
 
   // We have to define render in react
@@ -28,7 +35,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />
     }
     
     return <div>Loading...</div>;
